c66d93df85771c6365c81c60bfa38397db9e6850
SahilThakur198 <sahil.aarakh@gmail.com>
2026-02-27 06:54:29 +0530

    Initial commit - VendorCalc PWA

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sahil.vendorcalc',
+  appName: 'VendorCalc',
+  webDir: 'dist'
+};
+
+export default config;
